5aa781fafa04476c3898ec3862a1433fba996053
info@dgt-international.com <info@dgt-international.com>
2026-08-10 22:24:55 -0400

    show a spinner while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,12 @@
-import { List, ListItem } from "@chakra-ui/react";
+import { List, ListItem, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenre";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
+
   return (
     <List>
       {data.map((genre, index) => (
