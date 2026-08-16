9d9c6d081221c7d8d5bb398c9cf83528e6038524
info@dgt-international.com <info@dgt-international.com>
2026-08-16 08:52:41 -0400

    genre map

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,9 +9,24 @@ export interface Game {
   genre: string;
 }
 
+const genreMap: Record<string, string> = {
+  Shooter: "shooter",
+  MMORPG: "mmorpg",
+  RPG: "action-rpg",
+  Strategy: "strategy",
+  Racing: "racing",
+  Sports: "sports",
+};
+
 const useGames = (selectedGenre: Genre | null) =>
-  useData<Game>("/games", { params: { genres: selectedGenre?.genre } }, [
-    selectedGenre?.genre,
-  ]);
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        category: selectedGenre ? genreMap[selectedGenre.genre] : undefined,
+      },
+    },
+    [selectedGenre?.genre],
+  );
 
 export default useGames;
