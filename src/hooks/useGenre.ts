cdd54a20d6a797c9956658b987253fc093c7ae8e
info@dgt-international.com <info@dgt-international.com>
2026-08-16 14:56:30 -0400

    making genre list unique

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -4,6 +4,14 @@ export interface Genre {
   genre: string;
 }
 
-const useGenres = () => useData<Genre>("/games");
+const useGenres = () => {
+  const { data, error } = useData<Genre>("/games");
+
+  const uniqueGenres = [...new Set(data.map((item) => item.genre))].map(
+    (genre) => ({ genre }),
+  );
+
+  return { data: uniqueGenres, error };
+};
 
 export default useGenres;
